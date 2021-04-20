fd692583923b8c12964ef537b9eb6d997737c8ba
Nurlan Alakbarov <nurlan.alekberov@tutamail.com>
2021-04-20 11:08:50 +0200

    Add Product model and implemented basic product creation script

--- cli/product_create.php
+++ cli/product_create.php
@@ -1,1 +1,22 @@
 <?php declare(strict_types=1);
+
+require 'vendor/autoload.php';
+
+use App\Model\Product;
+use App\Storage\Writer;
+
+$writer = new Writer();
+
+$requiredFields = ['name', 'price'];
+
+$options = getopt("", ["name:", "price:"]);
+$missingFields = array_diff($requiredFields, array_keys($options));
+
+if (!empty($missingFields)) {
+    die('Please include name and price fields!');
+}
+
+$product = new Product($options['name'], $options['price']);
+
+$writer->create($product->getId() . '.json', json_encode($product));
+
